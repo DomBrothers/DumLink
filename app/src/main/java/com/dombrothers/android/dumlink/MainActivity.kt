b6e424ed605227b317b464e0d0b96fd7df3fe4b8
changs97 <kin2027@gmail.com>
2023-03-18 01:18:52 +0900

    Init: 프로젝트 생성

---
+++ app/src/main/java/com/dombrothers/android/dumlink/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dombrothers.android.dumlink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
